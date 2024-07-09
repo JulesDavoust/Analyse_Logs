c139585fc7c56667e0c17691c51f755ee9b3b137
Jules <jules.davoust@yahoo.fr>
2024-07-10 00:26:10 +0200

    finished ?

--- Vagrant/data/start_spark.sh
+++ Vagrant/data/start_spark.sh
@@ -1,5 +1,11 @@
+
+
+
 #!/bin/bash
 source ~/.bashrc
 cd /vagrant_data
 echo "Starting Spark at $(date)" >> /vagrant_data/spark_log.txt
 spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 /vagrant_data/spark_app2.py >> /vagrant_data/spark_log.txt 2>&1
+
+
+
